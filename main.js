64b1ec328fe847174944b52dddd1c22ce288a255
adik-creator <adduser1000@gmail.com>
2023-01-21 00:28:21 +0600

    joke

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let result = window.confirm("Ты пидр?")
+
+while (result !== true){
+    result = window.confirm("Ответь пидр .....")
+}
+
+if (result){
+    alert("Я знал ха ха хаааааа!!!")
+}
+
